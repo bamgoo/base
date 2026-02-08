55b8f46c25df66d96b1ae108dac5cfa31e933d63
yatniel <yatniel@qq.com>
2026-02-07 23:22:47 -0800

    init

---
+++ base.go
@@ -0,0 +1,49 @@
+package base
+
+//
+
+type (
+	Any = interface{}
+	Map = map[string]Any
+
+	Res interface {
+		OK() bool
+		Fail() bool
+		Code() int
+		State() string
+		Args() []Any
+		With(...Any) Res
+		Error() string
+	}
+
+	Vars = map[string]Var
+	Var  struct {
+		nil      bool
+		Type     string
+		Required bool
+		Nullable bool
+		Name     string
+		Text     string
+		Default  Any
+		Setting  Map
+		Options  Map
+		Children Vars
+		Encode   string
+		Decode   string
+		Empty    Res
+		Error    Res
+		Check    func(Any, Var) bool
+		Convert  func(Any, Var) Any
+	}
+)
+
+var (
+	Nil = Var{nil: true}
+)
+
+func (vvv *Var) Nil() bool {
+	if vvv == nil {
+		return true
+	}
+	return vvv.nil
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/bamgoo/base
+
+go 1.25.3
